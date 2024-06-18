ffd35a13e21134d18ea54117239fdd313dbded89
Bastian Gajardo <120329341+darkanthem21@users.noreply.github.com>
2024-06-18 10:12:43 -0400

    Add files via upload

---
+++ ayudantia_9/base_3.py
@@ -0,0 +1,20 @@
+def valida_secuencia(sec: str, delta: int, index: int = 0) -> bool:
+    pass
+
+def leer_secuencias_desde_archivo(archivo: str) -> list:
+    pass
+
+def main():
+    archivo = 'secuencias.txt'
+    delta = 2
+
+    try:
+        secuencias = leer_secuencias_desde_archivo(archivo)
+    except:
+        pass
+
+    for secuencia in secuencias:
+        pass
+
+if __name__ == "__main__":
+    main()
